c486d31e6b630d72ae51c8eb5843e990947a29cc
Marishka6769 <marishka160698@gmail.com>
2017-02-11 23:43:27 +0200

    final version of hw_3

--- js.js
+++ js.js
@@ -6,6 +6,8 @@
 $(function () {
     var $List_item = $('.bl-row');
     var Template_element = $('.harvesting1').html(); //дістає htlm код зсередини
+    var Template_element_1 = $('.harvesting').html();
+    var $List_item_1 = $('.Whynot.no_buy');
 
 
     console.log("Template", Template_element);
@@ -17,24 +19,69 @@ $(function () {
 
         var $quantity_label = $node.find(".bl-label");//знаходжу куди писати назву
 
-        $node.find(".name").text(title); //записую назву замість стандартної
+        var $node_1 = $(Template_element_1);
 
+        var buy=$node.find(".bl-buttons.no_buy");
+
+        var no_buy=$node.find(".bl-buttons.buy");
+        buy.click(function () {
+            $node.find(".bl-button").hide();  //приховати елемент
+            $node.find(".bl-button.buy").show();
+
+            $node_1.remove();
+            $(".Whynot.buy").append($node_1);
+            $node.find(".name.production").addClass("line");
+            $node_1.addClass("line");
+        });
+
+        no_buy.click(function () {
+            $node.find(".bl-button").show();
+            $node.find(".bl-button.buy").hide();
+            $node_1.remove();
+            $(".Whynot.no_buy").append($node_1);
+            $node.find(".name.production").removeClass("line");
+            $node_1.removeClass("line");
+        });
+
+        $node_1.find(".bl-lists").text(title);
+
+        $node.find(".name.production").text(title); //записую назву замість стандартної
+        $node.find(".name.redaction").val(title);
+        $node.find(".name.production").click(function () {
+            $node.find(".name.production").hide();
+            $node.find(".name.redaction").show();
+        });
+        $node.find(".name.redaction").keyup(function () {
+            var new_title=$node.find(".name.redaction").val();
+            $node_1.find(".bl-lists").text(new_title);
+            $node.find(".name.production").text(new_title);
+            $node.find(".name.redaction").val(new_title);
+            $node.find(".name.redaction").focus();
+        });
+        $node.find(".name.redaction").mouseleave(function () {
+            $node.find(".name.production").show();
+            $node.find(".name.redaction").hide();
+        });
         //Delete action
         $node.find(".bl-buttons1").click(function () {
             $node.remove();
+            $node_1.remove();
         });
 
         $List_item.append($node); //Add to the end of the list
-
+        $List_item_1.append($node_1);
 
         $node.find(".bl-plus").click(function () {
             quantity++;
             $quantity_label.text(quantity);
+            $node_1.find(".bl-lists1").text(quantity);
         });
         $node.find(".bl-minus").click(function () {
             if (quantity > 1) {
                 quantity--;
                 $quantity_label.text(quantity);
+                $node_1.find(".bl-lists1").text(quantity);
+
             }
 
         });
@@ -58,6 +105,7 @@ $(function () {
         addItem("Помідори");//title
         addItem("Сир");
         addItem("Печиво");
+
 
 });
 
